c16e7e7c643ee9e1a5d69d2e45f75ebb248f2390
Ernesto Manrique <ernestopmc11@gmail.com>
2026-02-25 20:44:02 -0500

    feat: Configuracion de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,23 @@ import { CommonModule } from './common/common.module';
 import { BookModule } from './book/book.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
     imports: [
 
+        //* Configuracion para habilitar el uso de variables de entorno
+        ConfigModule.forRoot({
+            load: [EnvConfiguration],
+            validationSchema: JoiValidationSchema
+        }),
+
         //* Conexion a MongoDB
-        MongooseModule.forRoot('mongodb://localhost:27018/biblioteca'),
+        MongooseModule.forRoot(process.env.MONGODB!, {
+            dbName: 'biblioteca'
+        }),
 
         //* Modulos
         AuthorModule,
